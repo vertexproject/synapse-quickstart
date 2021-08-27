ebfe82e9f76b098a8c8dd434e1c3fa86b19372fb
invisig0th <visi@vertex.link>
2021-08-27 06:34:08 -0400

    Update cortex.bat

--- cortex.bat
+++ cortex.bat
@@ -2,4 +2,4 @@ docker volume create --name=cortex00
 docker-compose pull
 docker-compose down
 docker-compose up -d
-cmd /k docker-compose exec cortex /bin/bash
+docker-compose exec cortex /bin/bash
